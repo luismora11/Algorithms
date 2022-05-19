db55962d3a8c81b24c7c45f8124e39a1073a63b9
Luis Morales <luismorales@Luiss-MBP.attlocal.net>
2022-05-18 20:13:56 -0500

    This is a class and an object

---
+++ object.py
@@ -0,0 +1,16 @@
+from tkinter import N
+
+
+class Person:
+    
+    def __init__(self, name, weight, age):
+        self.n = name
+        self.w = weight
+        self.a = age
+
+    def sayHi(self):
+            print("My name is " + self.n)
+
+p1 = Person("Luis", 149, 29)
+p1.sayHi() 
+        
